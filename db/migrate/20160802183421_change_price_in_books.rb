1503ddb4f9cba0d63fcff14f666fe0a1b81f62d3
Felipe Henao <fhenaop@gmail.com>
2016-08-02 13:37:14 -0500

    changed price pricision in books table

---
+++ db/migrate/20160802183421_change_price_in_books.rb
@@ -0,0 +1,5 @@
+class ChangePriceInBooks < ActiveRecord::Migration
+  def change
+  	change_column :books, :price, :decimal, :precision => 5, :scale => 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,5 @@
-.n# This file is auto-generated from the current state of the database. Instead
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725135620) do
+ActiveRecord::Schema.define(version: 20160802183421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160725135620) do
     t.string   "author"
     t.string   "publisher"
     t.string   "publication_year"
-    t.decimal  "price",              precision: 8, scale: 2
+    t.decimal  "price",              precision: 5, scale: 2
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
     t.string   "cover_file_name"
