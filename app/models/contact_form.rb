1fbf8e7a20fbff06bb66f7b487e98e35eb695066
Felipe Henao <fhenaop@gmail.com>
2016-08-02 13:42:23 -0500

    added email validation to contact_form

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,2 +1,6 @@
 class ContactForm < ActiveRecord::Base
+	validates :name, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+	                  format: { with: VALID_EMAIL_REGEX }
 end
