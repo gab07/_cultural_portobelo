51ab3b64fddb95157ba1d53eef5f1203129212f6
Felipe Henao <fhenaop@gmail.com>
2016-08-17 10:52:19 -0500

    devise gem 'for' 2 'permit' update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
    def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password, :email) }
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password, :email) }
+     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:password, :email) }
+     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :email) }
 
    end
 end
