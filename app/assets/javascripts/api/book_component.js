7e513544b7d5c67fa5969b1cda5116b076290e0c
Felipe Henao <fhenaop@gmail.com>
2016-08-28 16:23:14 -0500

    catscope added

--- app/assets/javascripts/api/book_component.js
+++ app/assets/javascripts/api/book_component.js
@@ -4,33 +4,51 @@ var BookComponent = function(collection){
 
 BookComponent.prototype.template = function(book){
 	var books = "books/"
-  var title = book.title;
-  var length = 25;
-  var truncateTitle = function (title) {
-    if (title.length > 25) {
-      title = title.substring(0, length) + ' ...'
-      return title
-    } else {
-      return title
-    };
-  } 
-  var title = truncateTitle(title);
+    var title = book.title;
+    var length = 45;
+    var truncateTitle = function (title) {
+        if (title.length > 45) {
+            title = title.substring(0, length) + ' ...'
+            return title
+        } else {
+            return title
+        };
+    } 
+    var title = truncateTitle(title);
+    var author = book.author
 
-  var bookTemplate = "<a href=" + books + book.id + ">"
-      bookTemplate += "<div class='box panel panel-default masonry-brick'>"
-      bookTemplate += "<img class='product-img' src="+ book.image_url + ">"
-      bookTemplate += "<div class='panel-body'>"
-      bookTemplate += "<p id='index_title'>"+ title + "</p>"
-      bookTemplate += "<p id='index_author'>" + book.author + "</p>"
-      bookTemplate += "<p id='index_price'>$"+ parseInt(book.price) + "</p>"
-    bookTemplate += "</div></div></a>"
-	return bookTemplate
-}
+    var bookTemplate = "<a href=" + books + book.id + ">"
+            bookTemplate += "<div class='col-xs-3'>"
+                bookTemplate += "<div class='well sm-well book-container'>"
+                    bookTemplate += "<div class='row'>"
+
+                        bookTemplate += "<div class='row'>"
+                            bookTemplate += "<div class='col-md-12 img-container'>"
+                                bookTemplate += "<img class='img img-responsive img-rounded well-image' src="+ book.image_url + ">"
+                            bookTemplate += "</div>"
+                        bookTemplate += "</div>" // image-container
+
+                        bookTemplate += "<div class='col-xs-12 book-info'>"
+                            bookTemplate += "<div class='row'>"
+                                bookTemplate += "<h6><strong>"+ title + "</strong></h6>"
+                            bookTemplate += "</div>" //book-title
+
+                            bookTemplate += "<div class='row'>"
+                                bookTemplate += "<p><strong>"+ author + "</strong></p>"
+                            bookTemplate += "</div>" // book-author
+                        bookTemplate += "</div>"
+
+                    bookTemplate += "</div>"
+                bookTemplate += "</div>"
+            bookTemplate += "</div>"
+        bookTemplate += "</a>"
+    	return bookTemplate
+    }
+
 BookComponent.prototype.render = function(){
   var component = this
-  $(".books").empty()
+  $(".books-wrapper").empty()
   this.collection.books.forEach(function(book){
-    $(".books").append(component.template(book)) && $('.books').change().masonry('reload');
+    $(".books-wrapper").append(component.template(book));
   })
-   
 }
