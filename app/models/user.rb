6bfb53ff963442a774dfd539bb5a24dbf84f83c1
Felipe Henao <fhenaop@gmail.com>
2016-11-08 17:11:51 -0500

    prevent new user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, uniqueness: true
+  validates :full_name, presence: true, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
+   	validate :full_name_length
+
+  def full_name_length
+  	fullname = full_name.split
+  	if fullname.size <= 1
+  		errors.add(:full_name, I18n.t(:full_name_error))
+  	end
+  end
 end
