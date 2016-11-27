a98090627fd7c1e94e7cfc482b67ec179ee6fe34
Felipe Henao <fhenaop@gmail.com>
2016-11-27 17:20:54 -0500

    search filter fixed

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,82 +1,96 @@
-$(document).ready(function () {
-  $('#all-books').click(function(event){
+ function listener() {
+  $('.search-field').on("click", function(){
     $('#search-f').val("")
     $('#search_form').submit()
   });
-  $('#codigos').click(function(event){
-    $('#search-f').val("códigos")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+    $('#search-f').val(keyword);
+    $('#search_form').submit();
   });
 
-  $('#derecho-ambiental').click(function(event){
-    $('#search-f').val("derecho ambiental")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#ciencias-politicas').click(function(event){
-    $('#search-f').val("ciencias politicas")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+   var keyword = $(this).attr('data-keyword');
+       $('#search-f').val(keyword);
+       $('#search_form').submit();
   })
 
-  $('#derecho-civil').click(function(event){
-   $('#search-f').val("derecho civil")
-   $('#search_form').submit()
+  $('.search-field').on("click", function(){
+   var keyword = $(this).attr('data-keyword');
+       $('#search-f').val(keyword);
+       $('#search_form').submit();
   })
 
-  $('#autores-nacionales').click(function(event){
-    $('#search-f').val("autores nacionales")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-maritimo').click(function(event){
-    $('#search-f').val("derecho maritimo")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-internacional').click(function(event){
-    $('#search-f').val("derecho internacional")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-constitucional').click(function(event){
-    $('#search-f').val("derecho constitucional")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-administrativo').click(function(event){
-    $('#search-f').val("derecho administrativo")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-mercantil').click(function(event){
-    $('#search-f').val("derecho mercantil")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-laboral').click(function(event){
-    $('#search-f').val("derecho laboral")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-de-familia').click(function(event){
-    $('#search-f').val("derecho de familia")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-penal').click(function(event){
-    $('#search-f').val("derecho penal")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-informatico').click(function(event){
-    $('#search-f').val("derecho informatico")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-  $('#derecho-juridico').click(function(event){
-    $('#search-f').val("derecho juridico")
-    $('#search_form').submit()
+  $('.search-field').on("click", function(){
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
-})
-
+};
