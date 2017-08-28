920eff65b9913294b7bc55a65401597ff8f9a691
stand87 <cgarciasantander@gmail.com>
2017-08-28 01:46:42 +0000

    #32 Add read more button

--- app/assets/javascripts/global/book.js
+++ app/assets/javascripts/global/book.js
@@ -1,4 +1,21 @@
- function listener() {
+$(document).on('turbolinks:load', function(){
+  var book__summary = $('.book__summary').text().length;
+  if(book__summary > 400){
+    var short_summary = $('.book__summary').text().slice(0, 400);
+    var long_summary = $('.book__summary').text().slice(400);
+    var template = '<span class="book__summary_hidden">%s</span>';
+    var hidden_section = template.replace('%s', long_summary);
+    $('.book__summary').text(short_summary);
+    $('.book__summary').append(hidden_section);
+  } else {
+    $('.book__read-more').hide();
+  }
+  $('.book__read-more').on('click', function(e){
+    e.preventDefault();
+    $('.book__summary_hidden').toggle();
+  });
+});
+function listener() {
   $('.search-field').on("click", function(){
     $('#search-f').val("")
     $('#search_form').submit()
@@ -16,15 +33,15 @@
   })
 
   $('.search-field').on("click", function(){
-   var keyword = $(this).attr('data-keyword');
-       $('#search-f').val(keyword);
-       $('#search_form').submit();
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
   $('.search-field').on("click", function(){
-   var keyword = $(this).attr('data-keyword');
-       $('#search-f').val(keyword);
-       $('#search_form').submit();
+    var keyword = $(this).attr('data-keyword');
+        $('#search-f').val(keyword);
+        $('#search_form').submit();
   })
 
   $('.search-field').on("click", function(){
